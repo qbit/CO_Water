63aa61df794f79694d8283ec66edae023dea7d52
Aaron Bieber <deftly@gmail.com>
2014-05-06 08:55:33 -0600

    ugly spagetti for gettin title

--- extract.js
+++ extract.js
@@ -1,3 +1,25 @@
 var cheerio = require('cheerio'),
 fs = require('fs'),
-pg = require('pg');
+pg = require('pg'),
+opath = __dirname + '/output/';
+
+fs.readdir(opath, function(e, files) {
+	if (e) {
+		throw e;
+	}
+	files.forEach(function(item, idx) {
+		if (item.match(/html$/)) {
+			fs.readFile(opath + item, {encoding: 'utf8'}, function(er, data) {
+				if (er && ! data) {
+					console.log(er);
+				} else {
+					var $ = cheerio.load(data),
+					title = $('font').children()[0];
+					if (title) {
+						console.log("TITLE: ", title.children[0].data.replace(/\n/g, ''));
+					}
+				}
+			});
+		}
+	});
+});
